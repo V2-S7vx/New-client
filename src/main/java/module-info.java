418b5b7fc199b327b7d96d4d8a8bd0953a83237f
V2-S7vx <hgriffiths659@gmail.com>
2026-09-02 02:17:50 +0100

    Add modules required for Microsoft authentication and browser sign-in

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,8 @@
 module dev.mapz.launcher {
     requires javafx.controls;
     requires javafx.graphics;
+    requires java.net.http;
+    requires java.desktop;
 
     exports dev.mapz.launcher;
 }
